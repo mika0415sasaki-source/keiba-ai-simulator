47fdc8841605557dcc5495a7f800080add5986bc
mika0415sasaki-source <mika0415sasaki@gmail.com>
2026-09-14 19:14:57 +0900

    Revert ineffective card content visibility fix

--- ios-render-fix-v355.js
+++ ios-render-fix-v355.js
@@ -42,10 +42,6 @@
           visibility:visible !important;
           opacity:1 !important;
         }
-        #horses > .card{
-          content-visibility:auto;
-          contain-intrinsic-size:auto 560px;
-        }
       }
     `;
     document.documentElement.dataset.iosRenderFix='v368';
